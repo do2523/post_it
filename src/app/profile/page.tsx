dba6d97006d01c5ae928b05df32d989d76c467d7
L <luparedes100@gmail.com>
2024-09-13 13:52:22 -0400

    stylized user profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,13 @@
+import Button from "../_components/button";
 import { ProfileComponent } from "../_components/user-profile";
 
 export default function Profile() {
     return (
-        <>
-            <ProfileComponent />
-        </>
+        <div className="bg-slate-500 text-white flex flex-col p-3 rounded-xl">
+            <div className="items-center justify-center flex">
+                <ProfileComponent />
+            </div>
+            <Button href="../"/>
+        </div>
     )
 }
