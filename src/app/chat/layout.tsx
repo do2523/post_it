e82e3ce2cdcc7396d81b52cbc5efe00180e4a505
L <luparedes100@gmail.com>
2024-09-14 19:09:50 -0400

    changed the background color of the chat layout

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -3,7 +3,7 @@ import { InputBar } from "./_components/input-bar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return(
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col h-full bg-slate-700">
             <div className="overflow-y-auto overflow-x-hidden">   
                 {children}
             </div>
