f40bba7bb0561281fa6e0661d6cb25ae0b9a486c
L <luparedes100@gmail.com>
2024-09-14 15:00:32 -0400

    added input bar to chat page layout

---
+++ src/app/chat/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { InputBar } from "../_components/input-bar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return(
+        <div className="flex flex-col h-full">
+            <div className="overflow-y-auto overflow-x-hidden">   
+                {children}
+            </div>
+            <InputBar />
+        </div>
+    )
+}
