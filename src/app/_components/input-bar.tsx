9f080344c0e94e79644d08a7512db785bd258a25
L <luparedes100@gmail.com>
2024-09-14 18:24:28 -0400

    pressing enter can now submit posts

--- src/app/_components/input-bar.tsx
+++ src/app/_components/input-bar.tsx
@@ -13,14 +13,18 @@ export function InputBar() {
         },
     });
 
+    const submitPost = () => {
+        if(text.trim() != "") {
+            createPost.mutate({ content: text });
+            setText("")
+        }
+    }
+
     return (
         <div className="sticky bottom-0 left-0 flex-row flex max-w-full">
-            <input value={text} onChange={(e) => setText(e.target.value)} placeholder="Send a message" className="p-1 w-full" type="text" />
-            <button onClick={() => {
-                createPost.mutate({ content: text });
-                setText("")
-
-            }} className="bg-black p-5 text-white">Submit</button>
+            <input value={text} onChange={(e) => setText(e.target.value)} placeholder="Send a message" className="p-1 w-full" type="text" onKeyDown={(keyDown) => {
+                if(keyDown.key === "Enter") { submitPost() }}}/>
+            <button onClick={() => {submitPost()}} className="bg-black p-5 text-white">Submit</button>
         </div>
     )
 }
